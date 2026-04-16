8fa25ddc3da05eeaaf7193a56a3702fe1bc1fd85
shriradhe <malikalone@gmail.com>
2026-04-17 00:15:08 +0530

    Initial commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -172,6 +172,7 @@ export default function Navbar() {
                   ))}
                 </div>
               </div>
+              )}
             </div>
 
             <NavLink to="/dashboard" className={({ isActive }) =>
